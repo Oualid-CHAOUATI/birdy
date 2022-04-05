8c134fd307f6159f69d3f5f0cec74e5df32988c8
Oualid <oualidchaouati3@gmail.com>
2022-04-05 11:14:31 +0200

    added routes will add pages

--- navigationPannel.js
+++ navigationPannel.js
@@ -1,68 +1,26 @@
-import React from 'react';
-import { FormsContext } from './pages/mainPage';
+import React from "react";
+import { FormsContext } from "./pages/mainPage";
 import { useContext } from "react";
 
-import"./navigationPannel.css"
-import { NavLink } from 'react-router-dom';
-function NavigationPannel ({userID}){
-    let ctxValue = useContext(FormsContext);
+import "./navigationPannel.css";
+import { NavLink } from "react-router-dom";
+function NavigationPannel({ userID }) {
+  let ctxValue = useContext(FormsContext);
 
-
-
-
-    const gotoFollowersPage=()=>{
-        ctxValue.gotoFollowersPage(userID)
-    }
-  
-    const gotoFollowingsPage=()=>{
-        ctxValue.gotoFollowingsPage(userID)
-    }
-  
-    const gotoProfilePage=()=>{
-        ctxValue.gotoProfilePage(userID)
-    }
-  
-    const gotoPostsPage=()=>{
-        ctxValue.gotoPostsPage(userID)
-    }
-  
-    const gotoConnectionPage=()=>{
-        ctxValue.gotoConnectionPage()
-    }
-  
-    const gotoInscriptionPage=()=>{
-        ctxValue.gotoInscriptionPage()
-    }
-  
-    const gotoLinksEditPage=()=>{
-        ctxValue.gotoLinksEditPage()
-    }
-  
-    const gotoSearchBar=()=>{
-        ctxValue.gotoSearchBar()
-    }
-  
-    
-    return <nav className="main-nav ">
-
-        <ul className="menu flex ">
-            <li onClick={gotoFollowersPage}>Followers</li>
-            <li onClick={gotoFollowingsPage}>Followings</li>
-            <li onClick={gotoProfilePage}>Profile</li>
-<NavLink to="/profile">
-    profile
-</NavLink>
-
-            <li onClick={gotoPostsPage}>Posts</li>
-            <li onClick={gotoConnectionPage}>connexion</li>
-            <li onClick={gotoInscriptionPage}>inscription</li>
-            <li onClick={gotoLinksEditPage}>LinksEdit</li>
-            <li onClick={gotoSearchBar}>Search</li>
-        </ul>
-     
+  return (
+    <nav className="main-nav ">
+      <ul className="menu flex  f-gap-1">
+        <NavLink to={`/profile/${userID}`}>profile</NavLink>
+        <NavLink to={`/profile/${userID}/posts`}>posts</NavLink>
+        <NavLink to={`/searchBar`}>searchbar</NavLink>
+        <NavLink to={`/followers/${userID}`}>followers</NavLink>
+        <NavLink to={`/followings/${userID}`}>followings</NavLink>
+        <NavLink to={`/connectionForm`}>connection</NavLink>
+        <NavLink to={`/inscriptionForm`}>inscription</NavLink>
+        <NavLink to={`/linksEditForm`}>NavLinknksEditForm</NavLink>
+      </ul>
     </nav>
-
-
+  );
 }
 
 export default NavigationPannel;
